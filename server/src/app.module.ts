31b0f6b04555541faed7153520515120ab8a8154
Daniel Rojas Asis <danielrojasasis@gmail.com>
2025-08-09 17:18:32 -0500

    prepare backend for deployment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
--- server/src/main.ts
+++ server/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT || 3000); 
+  await app.listen(process.env.PORT || 3000);
   logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
